fc6355fec3400600fe2bbebee1f8467e87218811
Yuvraj Singh <yuvi7860808034@gmail.com>
2023-04-02 10:37:52 +0530

    login and signup api

---
+++ controllers/user.controllers.js
@@ -0,0 +1,51 @@
+const User = require("../models/User.model.js");
+const bcrypt = require("bcrypt");
+
+const signUp = async (req, res) => {
+  try {
+    const { password, userName, email } = req.body;
+
+    const duplicate = await User.findOne({ email }).exec();
+    if (duplicate)
+      return res.status(409).json({ message: "Duplicate email present" });
+
+    // encrypt the password
+    const hashedPwd = await bcrypt.hash(password, 10);
+
+    // create and store the new user
+    const result = await User.create({
+      password: hashedPwd,
+      userName,
+      email,
+    });
+
+    res.status(201).json({ message: `New user created!`, result: result._id });
+  } catch (err) {
+    return res.status(500).json({ message: err.message }).end();
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const foundUser = await User.findOne({ email }).exec();
+    if (!foundUser)
+      return res.status(401).json({ message: "User does not exist." }); //Unauthorized
+
+    // evaluate password
+    const match = await bcrypt.compare(password, foundUser.password);
+
+    if (match) {
+      return res
+        .status(200)
+        .json({ message: "User Verified", userId: foundUser._id });
+    } else {
+      return res.status(400).json({ message: "Wrong password." });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message }).end();
+  }
+};
+
+module.exports = { signUp, login };
--- server.js
+++ server.js
@@ -1,26 +1,25 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/user.routes.js");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const startServer = async () => {
+app.use("/user", userRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on http://localhost:${process.env.PORT}`);
   try {
     // connect to the database
     mongoose
       .connect(process.env.MONGODB_URL)
       .then(() => console.log("MongoDB connected"))
       .catch((error) => console.log(error));
-    app.listen(process.env.PORT, () => {
-      console.log(`Server started on http://localhost:${process.env.PORT}`);
-    });
   } catch (e) {
     console.log(e);
   }
-};
-
-startServer();
+});
